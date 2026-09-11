9dc1d731ff568961538b0d0745e6d63af5fa1aae
Hojiakbar <namozovhojiajbar6@gmail.com>
2026-09-11 11:57:32 +0500

    API Platform setup completed

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,68 +2,31 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\ApiResource;
 
-#[ORM\Entity]
-#[ORM\Table(name: 'users')]
-#[ApiResource(
-    normalizationContext: ['groups' => ['user:read']],
-    denormalizationContext: ['groups' => ['user:write']]
-)]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource]
 class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['user:read'])]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
-    #[Groups(['user:read', 'user:write'])]
-    private ?string $email = null;
-
-    #[ORM\Column]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 100)]
-    #[Groups(['user:read', 'user:write'])]
+    #[ORM\Column(length: 255)]
     private ?string $fullname = null;
 
-    #[ORM\Column(length: 100)]
-    #[Groups(['user:read', 'user:write'])]
+    #[ORM\Column(length: 255)]
     private ?string $surname = null;
 
-    #[ORM\Column(type: 'integer')]
-    #[Groups(['user:read', 'user:write'])]
-    private ?int $age = null;
+    #[ORM\Column(length: 0)]
+    private ?string $age = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-        return $this;
     }
 
     public function getFullname(): ?string
@@ -71,9 +34,10 @@ class User
         return $this->fullname;
     }
 
-    public function setFullname(string $fullname): self
+    public function setFullname(string $fullname): static
     {
         $this->fullname = $fullname;
+
         return $this;
     }
 
@@ -82,20 +46,22 @@ class User
         return $this->surname;
     }
 
-    public function setSurname(string $surname): self
+    public function setSurname(string $surname): static
     {
         $this->surname = $surname;
+
         return $this;
     }
 
-    public function getAge(): ?int
+    public function getAge(): ?string
     {
         return $this->age;
     }
 
-    public function setAge(int $age): self
+    public function setAge(string $age): static
     {
         $this->age = $age;
+
         return $this;
     }
 }
